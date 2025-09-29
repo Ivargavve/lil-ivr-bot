d57ef529f3df8887133cc7ac278adfe0c03861ce
Ivar Gavelin <ivargavelin@gmail.com>
2025-09-29 08:58:32 +0200

    port change

--- extension/config.js
+++ extension/config.js
@@ -1,13 +1,13 @@
 // Configuration for Lil IVR Bot
 const CONFIG = {
   // Backend API URL - change this when deploying to production
-  API_BASE_URL: 'http://localhost:8000',
+  // API_BASE_URL: 'http://localhost:8000',
 
   // For production deployment, uncomment the line below and comment out the localhost line:
-  // API_BASE_URL: 'https://your-app-name.onrender.com',
+  API_BASE_URL: 'https://lil-ivr-bot.onrender.com',
 
   // Development mode flag
-  DEV_MODE: true
+  DEV_MODE: false
 };
 
 // Make config available globally
